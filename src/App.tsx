b90211e95854110b31d49a8fd473f1a825356b1d
James O'Kane <orbiccode@gmail.com>
2025-12-16 20:04:50 +0000

    imported Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <main>
-      <h1>PopChoice</h1>
+    <main className='container-main'>
+      <Header />
     </main>
   );
 }
