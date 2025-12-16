5c13a761322bd5306dac83e9c6f50d4f07a38969
James O'Kane <orbiccode@gmail.com>
2025-12-16 20:14:22 +0000

    imported PromptForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import PromptForm from './components/PromptForm/PromptForm';
 
 function App() {
   return (
     <main className='container-main'>
       <Header />
+      <PromptForm />
     </main>
   );
 }
