9c2d0d44da774ba93d8180d0a99beab8f0cd2f2f
James O'Kane <orbiccode@gmail.com>
2025-12-17 12:33:43 +0000

    added has prompted state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import PromptForm from './components/PromptForm/PromptForm';
+import Results from './components/Results/Results';
 
 function App() {
+  const [hasPromted, setHasPrompted] = useState(false);
   return (
     <main className='container-main'>
       <Header />
-      <PromptForm />
+      {hasPromted ? (
+        <Results setHasPrompted={setHasPrompted} />
+      ) : (
+        <PromptForm setHasPrompted={setHasPrompted} />
+      )}
     </main>
   );
 }
