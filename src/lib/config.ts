dcd9f7c65a293007cc81c406c4f721213eed17dc
James O'Kane <orbiccode@gmail.com>
2025-12-17 00:33:47 +0000

    dotenv integration

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,16 +1,19 @@
 import OpenAI from 'openai';
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 /** OpenAI config */
-if (!import.meta.env.VITE_OPENAI_API_KEY) throw new Error('OpenAI API key is missing or invalid.');
+if (!process.env.OPENAI_API_KEY) throw new Error('OpenAI API key is missing or invalid.');
 export const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
 /** Supabase config */
-const privateKey = import.meta.env.VITE_SUPABASE_API_KEY;
+const privateKey = process.env.SUPABASE_API_KEY;
 if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
-const url = import.meta.env.VITE_SUPABASE_URL;
+const url = process.env.SUPABASE_URL;
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
